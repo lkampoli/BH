ff95c909b0dbdc5e68bcac656fc9eeec216901f4
Lorenzo <lorenzo.campoli@unimelb.edu.au>
2023-06-14 18:15:24 +1000

    update 00 01 02

---
+++ 01_Src/load.sh
@@ -0,0 +1,6 @@
+#mc cp kOmegaSSTGEP_Rijhat_added_Cases.tar.gz bq2/bump
+#mc cp VT_NASA_BeVERLI_3D_Hill_Baseline_RANS_k_Omega_SST_Mes_Lev0_64mm.tar.gz bq2/bump
+#mc cp VT_NASA_BeVERLI_3D_Hill_Baseline_RANS_k_Omega_SST_Mes_Lev0_64mm_MeshB_run2_SELECTED.tar.gz bq2/bump
+#mc cp Re_h_650k.tar.gz bq2/bump
+mc cp VT_NASA_BeVERLI_3D_Hill_Baseline_RANS_k_Omega_SST_45Deg_Mesh_VT_11M_Re_650k.tar.gz bq2/bump
+
